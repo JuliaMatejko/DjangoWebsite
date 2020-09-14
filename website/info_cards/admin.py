d35706bbb254db4941ac2ac8d8886735b4919799
Julia Matejko <matejkojulia@gmail.com>
2020-09-15 00:17:04 +0200

    Changes in models and admin.py.

--- website/info_cards/admin.py
+++ website/info_cards/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BasicInfoCard, DetailInfoCard
 
-# Register your models here.
+admin.site.register(BasicInfoCard)
+admin.site.register(DetailInfoCard)
--- website/info_cards/models.py
+++ website/info_cards/models.py
@@ -1,16 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
 class Species(models.Model):
     species = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.species
+
+
 class Breed(models.Model):
     breed = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.breed
+
+
 class Sex(models.Model):
-    sex = models.BooleanField()
+    sex = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.sex
+
 
 class Size(models.Model):
     size = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.size
 
 
 class BasicInfoCard(models.Model):
@@ -22,11 +38,18 @@ class BasicInfoCard(models.Model):
     age = models.IntegerField()
     size = models.ForeignKey(Size, on_delete=models.RESTRICT)
 
+    def __str__(self):
+        return f'{self.name}, {self.date_of_registration}, {self.species}, {self.breed}, {self.sex}, {self.age}, {self.size}'
+
+    def was_added_recently(self):
+        return self.date_of_registration >= timezone.now() - datetime.timedelta(days=1)
 
 class DetailInfoCard(models.Model):
     basic_info_card = models.ForeignKey(BasicInfoCard, on_delete=models.CASCADE)
     description = models.CharField(max_length=2000)
 
+    def __str__(self):
+        return self.description
 
 
 

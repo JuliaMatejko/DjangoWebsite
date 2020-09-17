9c938311a8bca9afbb46dd56afa7d7049951bb85
Julia Matejko <matejkojulia@gmail.com>
2020-09-17 22:43:16 +0200

    Modified detail and index template. Changed url and views.py.

--- website/info_cards/urls.py
+++ website/info_cards/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'info_cards'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:infocard_id>/', views.detail, name='detail')
+    path('detail/<int:basicinfocard_id>/', views.detail, name='detail')
 ]
--- website/info_cards/views.py
+++ website/info_cards/views.py
@@ -1,16 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
 
-from .models import BasicInfoCard
+from .models import BasicInfoCard, DetailInfoCard
 
 def index(request):
     latest_added_infocards = BasicInfoCard.objects.order_by('-date_of_registration')[:5]
     context = {'latest_added_infocards': latest_added_infocards}
     return render(request, 'info_cards/index.html', context)
 
-def detail(request, infocard_id):
-    try:
-        infocard = BasicInfoCard.objects.get(pk=infocard_id)
-    except BasicInfoCard.DoesNotExist:
-        raise Http404("Infocard with this id does not exist")
-    return render(request, 'info_cards/index.html', {'infocard': infocard_id})
+def detail(request, basicinfocard_id):
+    detail_info_card = get_object_or_404(DetailInfoCard, basic_info_card_id=basicinfocard_id)
+    return render(request, 'info_cards/detail.html', {'detail_info_card': detail_info_card})
